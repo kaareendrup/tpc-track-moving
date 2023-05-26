9a79e340bbb07ab904bb9fd0383f32bbf20fbc97
joachimckh <jh@joachimhansen.dk>
2023-05-26 17:22:23 +0200

    add RNN and fix dataset length comment

---
+++ engine/gt_pt_rnn.py
@@ -0,0 +1,99 @@
+import sys
+
+from tpcutils.dataset_pt import TPCTreeCluster
+
+from networks.pytorch.nn_lightning import FcNet,LitClusterNet, LitRNN
+from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.metrics import roc_curve, auc
+import glob
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint, StochasticWeightAveraging
+from pytorch_lightning.loggers import TensorBoardLogger
+
+import ROOT
+
+import yaml
+import io
+
+from dotmap import DotMap
+
+
+
+def generalised_trainer_PT_clusters(**kwargs):
+
+    config = DotMap(yaml.safe_load(open('/Users/joachimcarlokristianhansen/st_O2_ML_SC_DS/TPC-analyzer/TPCTracks/py_dir/config/config_file.yml')))
+
+    if config.DATA_PARAMS.IS_ROOT:
+        print("Using the tpc-trackStudy file in ROOT format")
+        # train
+        file = ROOT.TFile.Open(config.PATHS.DATA_PATH_TRAIN)
+        dataset_train = TPCTreeCluster(file,transform=True,conf=config)
+        # valid
+        file_valid = ROOT.TFile.Open(config.PATHS.DATA_PATH_VALID)
+        dataset_valid = TPCTreeCluster(file_valid,transform=True,conf=config)
+
+
+
+
+    #dataset_train,dataset_valid = train_test_split(dataset,test_size=config.DATA_PARAMS.TEST_SIZE, random_state=config.DATA_PARAMS.RANDOM_STATE)
+
+    print("Initializing data loaders")
+    train_loader = DataLoader(dataset_train,
+                              batch_size=config.HYPER_PARAMS.BATCH_SIZE,
+                              shuffle=config.DATA_PARAMS.SHUFFLE_TRAIN,
+                              num_workers=config.DATA_PARAMS.NUM_WORKERS)
+    val_loader = DataLoader(dataset_valid,
+                            batch_size=config.HYPER_PARAMS.BATCH_SIZE,
+                            shuffle=config.DATA_PARAMS.SHUFFLE_VALID,
+                            num_workers=config.DATA_PARAMS.NUM_WORKERS,
+                            )
+
+    print("Initializing net")
+    #input shape: 7+nClustersSelected*3
+    model = LitRNN(dataset_train._shape(),config)
+
+    logger = TensorBoardLogger(name="logs",save_dir=config.PATHS.SAVE_PATH + '/' + config.PATHS.MODEL_DIR)
+    # training
+    trainer = pl.Trainer(
+                    num_nodes=config.PYTORCH_LIGHTNING_PARAMS.NUM_NODES,
+                    precision=config.PYTORCH_LIGHTNING_PARAMS.PRECISION,
+                    limit_train_batches=config.PYTORCH_LIGHTNING_PARAMS.LIMIT_TRAIN_BATCHES,
+                    accelerator=config.PYTORCH_LIGHTNING_PARAMS.ACCELERATOR,
+                    devices=config.PYTORCH_LIGHTNING_PARAMS.DEVICES,
+                    callbacks=[EarlyStopping(monitor="val_loss",
+                                             mode="min",
+                                             patience=config.HYPER_PARAMS.EARLY_STOPPING.PATIENCE,
+                                             verbose=0),
+                               ModelCheckpoint(dirpath = config.PATHS.SAVE_PATH + '/' + config.PATHS.MODEL_DIR,
+                                               filename =config.MODEL.NAME + '_' + '{epoch}-{val_loss:.2f}',
+                                               monitor="val_loss",
+                                               mode='min',
+                                               save_top_k=1),
+                               StochasticWeightAveraging(swa_lrs=config.HYPER_PARAMS.SWA_LRS),
+                               ],
+                    max_epochs=config.HYPER_PARAMS.MAX_EPOCHS,
+                    logger=logger
+                    )
+
+    print("Attempting to fit")
+    trainer.fit(model, train_loader, val_loader,)
+
+    print("Fit finished successfully, dumping config to json...")
+    with io.open(config.PATHS.SAVE_PATH + '/' + config.PATHS.MODEL_DIR + '/hyperparams.yml', 'w', encoding='utf8') as outfile:
+        yaml.dump(config.toDict(),outfile)
+
+
+
+
+
+
+if __name__=='__main__':
+
+    generalised_trainer_PT_clusters()
--- networks/pytorch/nn_lightning.py
+++ networks/pytorch/nn_lightning.py
@@ -6,7 +6,7 @@ from torch.nn import functional as F
 import pytorch_lightning as pl
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from networks.pytorch.nn_networks import FcNet,DeepConvSimpleNet, PseudoGraph
+from networks.pytorch.nn_networks import FcNet,DeepConvSimpleNet, PseudoGraph, mRNN
 from tpcutils.training_pt import PiecewiseLinearLR, LogCoshLoss, VonMisesFisher2DLoss
 from torch.optim import Adam
 
@@ -188,4 +188,56 @@ class PseudoGraphNet(pl.LightningModule):
         angloss = self._loss2( torch.cat((logits[:,2].unsqueeze(1), logits[:,5].unsqueeze(1)), dim=1), y[:,2].unsqueeze(1) )
 
         loss = torch.cat((linloss, angloss.unsqueeze(1)), dim=1).mean()
+        self.log('val_loss', loss,on_step=False,on_epoch=True,prog_bar=True,logger=True)
+
+
+class LitRNN(pl.LightningModule):
+    def __init__(self,input_shape,config,**kwargs):
+        super().__init__()
+
+        self.net = mRNN(input_size=input_shape, output_size=config.MODEL.OUTPUT_SHAPE, hidden_dim=50, n_layers=4)
+
+        self.LEARNING_RATE = config.HYPER_PARAMS.LEARNING_RATE
+
+        self.lr_scheduler_patience = config.HYPER_PARAMS.LR_SCHEDULER_PATIENCE
+        self.lr_scheduler_factor = config.HYPER_PARAMS.LR_SCHEDULER_FACTOR_
+
+        self.save_hyperparameters()
+
+    def forward(self, x):
+        return self.net(x)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            params=self.parameters(),
+            lr=self.LEARNING_RATE,
+        )
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            patience=self.lr_scheduler_patience,
+            verbose=True,
+            factor=self.lr_scheduler_factor
+        )
+        return {
+           'optimizer': optimizer,
+           'scheduler': scheduler,
+           'monitor': 'val_loss'
+       }
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        x = x.unsqueeze(0)
+        logits,hn = self.forward(x)
+        
+        loss = F.mse_loss(logits, y)
+        self.log('train_loss', loss,on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        x = x.unsqueeze(0)
+        logits,hn = self.forward(x)
+
+        loss = F.mse_loss(logits, y)
+
         self.log('val_loss', loss,on_step=False,on_epoch=True,prog_bar=True,logger=True)
